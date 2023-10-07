cf76c450aef2a8d7ed7fd18f57959ce5e1b0d5d5
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-10-07 20:07:10 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import HomePage from "./Routes/HomePage";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <>TypeScript + React</>;
+  return (
+    <>
+      TypeScript + React
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Routes/HomePage.tsx
@@ -0,0 +1,8 @@
+import * as css from "../Styles/HomePageStyles";
+import { Box } from "@chakra-ui/react";
+
+const HomePage = () => {
+  return <Box css={css.Outer}> HomePage </Box>;
+};
+
+export default HomePage;
